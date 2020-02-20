fa31beb703a9662036b567d4ca84e07e88bd9995
lucasGabrielDeAA <lg.technus@gmail.com>
2020-02-20 09:59:36 -0300

    yup's file config added

---
+++ src/config/yup.js
@@ -0,0 +1,26 @@
+import * as Yup from 'yup';
+import CPF from 'gerador-validador-cpf';
+
+Yup.setLocale({
+  mixed: {
+    required: 'Campo obrigatório',
+    typeError: 'Valor inválido',
+  },
+  string: {
+    email: 'Preencha um e-mail válido',
+  },
+  number: {
+    min: 'O numero informado deve ser no mínimo ${min}',
+    integer: 'Este número deve ser um inteiro',
+    typeError: 'O valor precisa ser um número válido',
+  },
+  array: {
+    required: 'Esta lista naão pode estar vazia',
+  },
+});
+
+Yup.addMethod(Yup.string, 'cpf', () => {
+  return Yup.mixed().test('cpf', 'CPF inválido', value => CPF.validate(value));
+});
+
+export default Yup;
