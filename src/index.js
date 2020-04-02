444ac1b2176f54482a6f15c525819f7591b5bc54
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-02 15:44:44 -0300

    General adjustments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-import React, {useRef, useCallback, useEffect, useState} from 'react';
+import React, {useRef, useCallback, useState} from 'react';
 import {
   StyleSheet,
   Text,
   Alert,
   SafeAreaView,
   TouchableOpacity,
-  ScrollView,
 } from 'react-native';
 
 import {Scope} from '@unform/core';
 
 import Yup from './config/yup';
 
-import {Form} from '@unform/mobile';
+import Form from './components/Form';
 import Input from './components/Input';
 import MaskedInput from './components/MaskedInput';
-
-import KeyboardEventListener from './common/KeyboardEventListener';
 
 import './config/ReactotronConfig';
 
@@ -37,25 +34,8 @@ const schema = Yup.object().shape({
 
 export default function App() {
   const formRef = useRef(null);
-  const scrollRef = useRef(null);
 
   const [inputSelected, setInputSelected] = useState('');
-  const [keyboardHeight, setKeyboardHeight] = useState(-1);
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    if (inputSelected !== '' && keyboardHeight > 0) {
-      formRef.current
-        .getFieldRef(inputSelected)
-        .measure((fx, fy, width, height, px, py) => {
-          scrollRef.current.scrollTo({
-            x: 0,
-            y: offset + py,
-            animated: true,
-          });
-        });
-    }
-  }, [inputSelected, keyboardHeight]);
 
   const handleSubmit = useCallback(async (data, {reset}) => {
     try {
@@ -69,23 +49,17 @@ export default function App() {
 
       reset();
     } catch (err) {
-      // const errors = {};
-      // if (err instanceof Yup.ValidationError) {
-      //   err.inner.map(error => {
-      //     errors[error.path] = error.message;
-      //   });
-      //   formRef.current.setErrors(errors);
-      // }
+      const errors = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.map(error => {
+          errors[error.path] = error.message;
+        });
+        formRef.current.setErrors(errors);
+      }
     }
   }, []);
 
-  const handleCallback = useCallback(({keyboardHeight: height}) => {
-    // if (height !== 0) {
-    setKeyboardHeight(height);
-    // }
-  }, []);
-
-  const scrollToInput = useCallback(inputName => {
+  const scrollToFocusedInput = useCallback(inputName => {
     setInputSelected(inputName);
   }, []);
 
@@ -93,92 +67,87 @@ export default function App() {
     formRef.current.getFieldRef(inputName).focus();
   }, []);
 
-  const handleScroll = useCallback(event => {
-    setOffset(event.nativeEvent.contentOffset.y);
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView
-        ref={scrollRef}
-        style={styles.content}
-        contentContainerStyle={{
-          ...styles.contentContainer,
-          paddingBottom: keyboardHeight,
-        }}
-        onScroll={handleScroll}>
-        <KeyboardEventListener callback={handleCallback}>
-          <Form ref={formRef} onSubmit={handleSubmit}>
-            <Input
-              autoCorrect={false}
-              name="name"
-              placeholder="Name"
-              autoCapitalize="none"
-              returnKeyType="next"
-              onFocus={() => scrollToInput('name')}
-              onSubmitEditing={() => focusNextInput('email')}
-            />
+      <Form
+        formRef={formRef}
+        handleSubmit={handleSubmit}
+        inputSelected={inputSelected}
+        initialData={{
+          name: 'vinicius',
+          email: 'vini@catafesta@gmail.com',
+          age: '23',
+          password: '123456',
+          cpf: '11111111111',
+          birthday: '11111995',
+        }}>
+        <Input
+          autoCorrect={false}
+          name="name"
+          placeholder="Name"
+          autoCapitalize="none"
+          returnKeyType="next"
+          onFocus={() => scrollToFocusedInput('name')}
+          onSubmitEditing={() => focusNextInput('email')}
+        />
 
-            <Input
-              autoCorrect={false}
-              name="email"
-              placeholder="E-mail"
-              autoCapitalize="none"
-              // keyboardType="email-address"
-              returnKeyType="next"
-              onFocus={() => scrollToInput('email')}
-              onSubmitEditing={() => focusNextInput('age')}
-            />
+        <Input
+          autoCorrect={false}
+          name="email"
+          placeholder="E-mail"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          returnKeyType="next"
+          onFocus={() => scrollToFocusedInput('email')}
+          onSubmitEditing={() => focusNextInput('age')}
+        />
 
-            <Input
-              autoCorrect={false}
-              name="age"
-              placeholder="Age"
-              autoCapitalize="none"
-              keyboardType="numeric"
-              returnKeyType="done"
-              onSubmitEditing={() => focusNextInput('password')}
-            />
+        <Input
+          autoCorrect={false}
+          name="age"
+          placeholder="Age"
+          autoCapitalize="none"
+          keyboardType="numeric"
+          returnKeyType="done"
+          onSubmitEditing={() => focusNextInput('password')}
+        />
 
-            <Input
-              secureTextEntry
-              autoCorrect={false}
-              name="password"
-              placeholder="Password"
-              returnKeyType="next"
-              onSubmitEditing={() => focusNextInput('documents.cpf')}
-            />
+        <Input
+          secureTextEntry
+          autoCorrect={false}
+          name="password"
+          placeholder="Password"
+          returnKeyType="next"
+          onSubmitEditing={() => focusNextInput('documents.cpf')}
+        />
 
-            <Scope path="documents">
-              <MaskedInput
-                mask="cpf"
-                name="cpf"
-                placeholder="User's CPF"
-                keyboardType="numeric"
-                returnKeyType="done"
-                onSubmitEditing={() => focusNextInput('documents.birthday')}
-              />
+        <Scope path="documents">
+          <MaskedInput
+            mask="cpf"
+            name="cpf"
+            placeholder="User's CPF"
+            keyboardType="numeric"
+            returnKeyType="done"
+            onSubmitEditing={() => focusNextInput('documents.birthday')}
+          />
 
-              <MaskedInput
-                mask="datetime"
-                name="birthday"
-                placeholder="User's birthday"
-                keyboardType="number-pad"
-                returnKeyType="done"
-                options={{
-                  format: 'DD/MM/YYYY',
-                }}
-                onSubmitEditing={() => formRef.current.submitForm()}
-                onBlur={() => setKeyboardHeight(0)}
-              />
-            </Scope>
+          <MaskedInput
+            mask="datetime"
+            name="birthday"
+            placeholder="User's birthday"
+            keyboardType="number-pad"
+            returnKeyType="done"
+            options={{
+              format: 'DD/MM/YYYY',
+            }}
+            onSubmitEditing={() => formRef.current.submitForm()}
+          />
+        </Scope>
 
-            <TouchableOpacity onPress={() => formRef.current.submitForm()}>
-              <Text>Save</Text>
-            </TouchableOpacity>
-          </Form>
-        </KeyboardEventListener>
-      </ScrollView>
+        <TouchableOpacity onPress={() => formRef.current.submitForm()}>
+          <Text>Save</Text>
+        </TouchableOpacity>
+      </Form>
     </SafeAreaView>
   );
 }
@@ -189,11 +158,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginHorizontal: 15,
-  },
-  content: {
-    width: '100%',
-  },
-  contentContainer: {
-    paddingTop: 500,
   },
 });
