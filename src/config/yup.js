3937e908c4b5d1f62000915cde65f55cc177b69c
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-03 18:00:43 -0300

    Adjustments on yup's config file

--- src/config/yup.js
+++ src/config/yup.js
@@ -10,12 +10,12 @@ Yup.setLocale({
     email: 'Preencha um e-mail válido',
   },
   number: {
-    min: 'O numero informado deve ser no mínimo ${min}',
+    min: 'O número informado deve ser no mínimo ${min}',
     integer: 'Este número deve ser um inteiro',
     typeError: 'O valor precisa ser um número válido',
   },
   array: {
-    required: 'Esta lista naão pode estar vazia',
+    required: 'Esta lista não pode estar vazia',
   },
 });
 
