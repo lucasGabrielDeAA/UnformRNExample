f2037d487cbf007d25cfc6b740207827211c8ccb
vinicfrancisco <vini.ibi@gmail.com>
2020-04-03 15:01:58 -0300

    Added styled-components to masked-input

--- src/components/Form/MaskedInput/index.js
+++ src/components/Form/MaskedInput/index.js
@@ -1,7 +1,7 @@
 import React, {useRef, useEffect, useState, useCallback} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {TextInputMask} from 'react-native-masked-text';
 import {useField} from '@unform/core';
+
+import {Container, StyledInput, Error} from './styles';
 
 export default function MaskedInput({
   name,
@@ -44,13 +44,12 @@ export default function MaskedInput({
   }, [fieldName, registerField]);
 
   return (
-    <View style={styles.container}>
-      <TextInputMask
+    <Container>
+      <StyledInput
         underlineColorAndroid="transparent"
         type={mask}
-        style={
-          active ? styles.active : error ? styles.inputError : styles.input
-        }
+        active={active}
+        error={error}
         ref={inputRef}
         placeholder={placeholder}
         value={currentValue}
@@ -61,50 +60,7 @@ export default function MaskedInput({
         {...rest}
       />
 
-      {error && <Text style={styles.error}>{error}</Text>}
-    </View>
+      {error && <Error>{error}</Error>}
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    marginBottom: 15,
-    width: '100%',
-  },
-
-  input: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderWidth: 0,
-    color: '#444',
-    fontSize: 15,
-    paddingHorizontal: 12,
-    paddingVertical: 16,
-  },
-
-  inputError: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderColor: '#f00',
-    borderWidth: 0,
-    color: '#444',
-    fontSize: 15,
-    paddingHorizontal: 12,
-    paddingVertical: 16,
-  },
-
-  error: {
-    color: '#f00',
-    fontSize: 15,
-    marginTop: 5,
-  },
-
-  active: {
-    borderBottomWidth: 1 + StyleSheet.hairlineWidth,
-    borderColor: '#0050ff',
-    borderWidth: 0,
-    color: '#444',
-    fontSize: 18,
-    paddingHorizontal: 10,
-    paddingVertical: 13,
-  },
-});
---
+++ src/components/Form/MaskedInput/styles.js
@@ -0,0 +1,45 @@
+import styled, {css} from 'styled-components/native';
+import {StyleSheet} from 'react-native';
+import {TextInputMask} from 'react-native-masked-text';
+
+export const Container = styled.View`
+  flex-direction: column;
+  margin-bottom: 15px;
+  width: 100%;
+`;
+
+export const StyledInput = styled(TextInputMask)`
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  border-width: 0;
+  color: #444;
+  font-size: 15px;
+  padding: 16px 12px;
+
+  ${props =>
+    props.active &&
+    css`
+      border-bottom-width: ${1 + StyleSheet.hairlineWidth}px;
+      border-color: #0050ff;
+      border-width: 0;
+      color: #444;
+      font-size: 18px;
+      padding: 13px 10px;
+    `}
+
+  ${props =>
+    props.error &&
+    css`
+      border-bottom-width: ${StyleSheet.hairlineWidth}px;
+      border-color: #f00;
+      border-width: 0;
+      color: #444;
+      font-size: 15px;
+      padding: 16px 12px;
+    `}
+`;
+
+export const Error = styled.Text`
+  color: #f00;
+  font-size: 15px;
+  margin-top: 5px;
+`;
