e166ec55fda6abf30cee5ee26e8b1b7ee446be56
vinicfrancisco <vini.ibi@gmail.com>
2020-04-03 11:12:52 -0300

    keyboard top bar example

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,15 +1,34 @@
 import React, {useCallback, useEffect, useState, useRef} from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  LayoutAnimation,
+  StyleSheet,
+  Image,
+  View,
+  Platform,
+  UIManager,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import Form from '../components/Form';
 import Input from '../components/Input';
 
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 export default function CreatePost() {
   const formRef = useRef(null);
+  // const keyboard = useKeyboard();
 
   const [inputSelected, setInputSelected] = useState('');
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+  const [post, setPost] = useState('');
 
   const handleSubmit = useCallback(async (data, {reset}) => {
     try {
@@ -25,39 +44,89 @@ export default function CreatePost() {
     formRef.current.getFieldRef(inputName).focus();
   }, []);
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Form
-        formRef={formRef}
-        handleSubmit={handleSubmit}
-        inputSelected={inputSelected}
-        initialData={{
-          title: '',
-          description: '',
-        }}>
-        <Input
-          autoCorrect={false}
-          name="title"
-          placeholder="Post's title"
-          autoCapitalize="none"
-          returnKeyType="next"
-          onFocus={() => scrollToFocusedInput('title')}
-          onSubmitEditing={() => focusNextInput('email')}
-        />
+  const handleKeyboardLayout = useCallback(
+    ({height, layoutAnimationConfig}) => {
+      LayoutAnimation.configureNext(layoutAnimationConfig);
+      setKeyboardHeight(height);
+    },
+    [],
+  );
 
-        <Input
-          multiline
-          autoCorrect={false}
-          numberOfLines={5}
-          name="description"
-          placeholder="Post's description"
-          autoCapitalize="none"
-          returnKeyType="done"
-          onFocus={() => scrollToFocusedInput('description')}
-          onSubmitEditing={() => handleSubmit()}
-        />
-      </Form>
-    </SafeAreaView>
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <Form
+          handleKeyboardLayout={handleKeyboardLayout}
+          formRef={formRef}
+          handleSubmit={handleSubmit}
+          inputSelected={inputSelected}
+          initialData={{
+            title: '',
+            description: '',
+          }}>
+          <Image
+            source={{
+              uri:
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+            }}
+            resizeMode="contain"
+            style={styles.logo}
+          />
+
+          <Input
+            autoCorrect={false}
+            name="title"
+            placeholder="Post's title"
+            keyboardType="number-pad"
+            autoCapitalize="none"
+            onFocus={() => scrollToFocusedInput('title')}
+            onSubmitEditing={() => focusNextInput('description')}
+          />
+
+          <Input
+            multiline
+            autoCorrect={false}
+            numberOfLines={5}
+            name="description"
+            placeholder="Post's description"
+            autoCapitalize="none"
+            returnKeyType="done"
+            onFocus={() => scrollToFocusedInput('description')}
+            onSubmitEditing={() => handleSubmit()}
+            value={post}
+            onChangeText={value => setPost(value)}
+            maxLength={240}
+          />
+        </Form>
+      </SafeAreaView>
+
+      <View
+        style={{
+          ...styles.topKeyboard,
+          bottom: keyboardHeight,
+          display: keyboardHeight > 0 ? 'flex' : 'none',
+        }}>
+        {inputSelected === 'title' ? (
+          <>
+            <TouchableOpacity onPress={() => {}}>
+              <Text>Clean</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => focusNextInput('description')}>
+              <Text>Next</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <Text style={styles.textCount}>{post.length} / 240</Text>
+
+            <TouchableOpacity onPress={() => setPost('')}>
+              <Text>Clean</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+    </>
   );
 }
 
@@ -67,6 +136,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginHorizontal: 15,
-    paddingTop: 250,
+  },
+  logo: {
+    alignSelf: 'center',
+    height: 200,
+    width: 300,
+    marginTop: 50,
+    marginBottom: 50,
+  },
+  topKeyboard: {
+    alignItems: 'center',
+    backgroundColor: '#dfdfdf',
+    flexDirection: 'row',
+    height: 50,
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    position: 'absolute',
+    width: '100%',
+  },
+  textCount: {
+    color: '#000',
+    fontSize: 14,
+    fontWeight: '400',
   },
 });
