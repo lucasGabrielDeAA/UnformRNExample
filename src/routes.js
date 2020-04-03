5ffd66eb224aa8dd518ad920d78b5f8237d49fd3
vinicfrancisco <vini.ibi@gmail.com>
2020-04-03 16:20:10 -0300

    fixed page headers

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,21 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from './pages/Home';
 import Submit from './pages/Submit';
 import CreatePost from './pages/CreatePost';
+import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator initialRouteName="Home" headerMode="none">
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
       <Stack.Screen name="Submit" component={Submit} />
       <Stack.Screen name="CreatePost" component={CreatePost} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
 }
