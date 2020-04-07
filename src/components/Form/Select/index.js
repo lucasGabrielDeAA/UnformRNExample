84943132f0232bdf8feb1b0d132587960dd4da13
vinicfrancisco <vini.ibi@gmail.com>
2020-04-07 10:21:15 -0300

    added picker to select component and fixed platform problems

--- src/components/Form/Select/index.js
+++ src/components/Form/Select/index.js
@@ -1,25 +1,89 @@
 import React, {useState, useCallback} from 'react';
-import {Text} from 'react-native';
+import {Platform} from 'react-native';
 import Modal from 'react-native-modal';
 
-import {Container, Label, ModalContainer} from './styles';
+import {
+  Container,
+  Label,
+  Value,
+  StyledPicker,
+  ModalContainer,
+  ModalTitle,
+  Buttons,
+  Button,
+  CancelText,
+  ConfirmText,
+} from './styles';
 
-export default function Select({placeholder}) {
+export default function Select({placeholder, options}) {
   const [modalShown, setModalShown] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const handleSelect = useCallback(
+    (itemValue, itemIndex) => {
+      setSelected(options[itemIndex]);
+    },
+    [options],
+  );
+
+  const handleCancel = useCallback(() => {
+    setSelected(null);
+    setModalShown(false);
+  }, []);
+
+  const handlePress = useCallback(() => {
+    if (Platform.OS === 'ios') {
+      setModalShown(true);
+    }
+  }, []);
 
   return (
     <>
-      <Container onPress={() => setModalShown(!modalShown)}>
+      <Container disabled={Platform.OS === 'android'} onPress={handlePress}>
         <Label>{placeholder}</Label>
+
+        {Platform.OS === 'ios' ? (
+          <Value>{selected?.label || ''}</Value>
+        ) : (
+          <StyledPicker
+            selectedValue={selected?.value || ''}
+            onValueChange={handleSelect}>
+            {options.map(option => (
+              <StyledPicker.Item label={option.label} value={option.value} />
+            ))}
+          </StyledPicker>
+        )}
       </Container>
 
       <Modal
         isVisible={modalShown}
         onBackdropPress={() => setModalShown(false)}>
         <ModalContainer>
-          <Text>testando</Text>
+          <ModalTitle>{placeholder}</ModalTitle>
+          <StyledPicker
+            selectedValue={selected?.value || ''}
+            onValueChange={handleSelect}>
+            {options.map(option => (
+              <StyledPicker.Item label={option.label} value={option.value} />
+            ))}
+          </StyledPicker>
+
+          <Buttons>
+            <Button onPress={handleCancel}>
+              <CancelText>Cancelar</CancelText>
+            </Button>
+
+            <Button onPress={() => setModalShown(false)}>
+              <ConfirmText>OK</ConfirmText>
+            </Button>
+          </Buttons>
         </ModalContainer>
       </Modal>
     </>
   );
 }
+
+Select.defaultProps = {
+  placeholder: '',
+  options: [],
+};
--- src/components/Form/Select/styles.js
+++ src/components/Form/Select/styles.js
@@ -1,13 +1,15 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
 import styled from 'styled-components/native';
+import {Picker} from '@react-native-community/picker';
 
 export const Container = styled.TouchableOpacity`
+  align-items: center;
   background: #fff;
   border-bottom-width: ${StyleSheet.hairlineWidth}px;
   border-color: ${props => (props.error ? '#f00' : '#ddd')};
-  flex-direction: column;
+  flex-direction: row;
   height: ${props => (props.multiline ? '250px' : '40px')};
-  justify-content: center;
+  justify-content: space-between;
   padding: 0 15px;
   width: 100%;
 `;
@@ -16,12 +18,52 @@ export const Label = styled.Text`
   border-width: 0;
   color: #b4b4b4;
   font-size: 15px;
-  width: 100%;
+`;
+
+export const Value = styled.Text`
+  border-width: 0;
+  color: #000;
+  font-size: 15px;
+`;
+
+export const StyledPicker = styled(Picker)`
+  width: ${Platform.OS === 'ios' ? '100%' : '40%'};
 `;
 
 export const ModalContainer = styled.View`
   align-self: center;
+  align-items: center;
   background: #fff;
-  height: 400px;
-  width: 70%;
+  border-radius: 4px;
+  box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.4);
+  justify-content: center;
+  padding-top: 20px;
+  width: 90%;
+`;
+
+export const ModalTitle = styled.Text`
+  color: #000;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const Buttons = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const Button = styled.TouchableOpacity`
+  align-items: center;
+  height: 50px;
+  justify-content: center;
+  width: 50%;
+`;
+
+export const CancelText = styled.Text`
+  font-size: 16px;
+`;
+
+export const ConfirmText = styled.Text`
+  color: #ff4400;
+  font-size: 16px;
 `;
